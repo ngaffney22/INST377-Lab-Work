928f15da1caa9940256a8563e9f6fd150e1396a8
ngaffney22 <74672972+ngaffney22@users.noreply.github.com>
2021-09-20 22:55:19 -0400

    set up bird movment and spacing

---
+++ client/Flappy/scripts.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded' , () =>{
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+
+    let birdLeft = 220
+    let birdBottom = 100
+    let gravity = 2
+
+    function startGame(){
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
+    }
+    let timerId = setInterval(startGame(), 20)
+
+    
+})
