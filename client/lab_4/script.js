00e40a6eca986432a3dd7e9e3c7adace8c8534f7
ngaffney22 <74672972+ngaffney22@users.noreply.github.com>
2021-09-25 16:31:26 -0400

    Final edits to JS for Carousel

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,3 +2,35 @@ let slidePosition = 0;
 const slides = document.querySelectorAll(".carousel-item");
 const totalSlides = slides.length;
 
+document.querySelector('.button-next').addEventListener("click", function() {
+    moveNextSlide();
+})
+document.querySelector('.button-prev').addEventListener("click", function() {
+    movePrevSlide();
+})
+
+function updateSlidePosition(){
+    for (let slide of slides){
+        slide.classList.remove('carousel-item--visable');
+        slide.classList.add('carousel-item--hidden');
+    }
+    slides[slidePosition].classList.add('carousel-item--visable');
+}
+function moveNextSlide() {
+    if (slidePosition === totalSlides - 1){
+        slidePosition = 0;
+    }else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+function movePrevSlide() {
+    if (slidePosition === 0){
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+}
