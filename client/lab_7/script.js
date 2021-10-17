c9cadabd82795a39909a56bcd0359d4717e3b354
ngaffney22 <74672972+ngaffney22@users.noreply.github.com>
2021-10-17 14:48:32 -0400

    got api working with map display

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,9 +1,8 @@
 async function windowActions() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  
-    
   const request = await fetch(endpoint)
   const cities = await request.json()
+  //const accessToken = 'pk.eyJ1IjoibmpnYWZmbmV5IiwiYSI6ImNrdXZmaHU0cTY3cDEzMG1hd3ZoN3RkbTAifQ.4SQUMvS57EWb7rrXbrdQPA'
   
   function findMatches(wordToMatch, cities) {
     return cities.filter(place => {
@@ -14,7 +13,8 @@ async function windowActions() {
     });
   
   }
-  const mymap = L.map('mapid')
+  
+  const mymap = L.map('mapid').setView([38.989, -76.93], 11);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -22,13 +22,14 @@ async function windowActions() {
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibmpnYWZmbmV5IiwiYSI6ImNrdXZmaHU0cTY3cDEzMG1hd3ZoN3RkbTAifQ.4SQUMvS57EWb7rrXbrdQPA'
+  
   }).addTo(mymap);
   function centerLeaflet(map, marker) {
     let latLngs = [marker.getLatLng()];
     let markerBounds = L.latLngBounds(latLngs);
     map.fitBounds(markerBounds);
   }
-  
+  //centerLeaflet()
 
   const searchInput = document.querySelector('.input');
   const suggestions = document.querySelector('.suggestions');
